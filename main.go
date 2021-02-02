a3602329467a0dcc4607e2ce8ce9663c4d81ab3f
kevo <kevinbright1994@gmail.com>
2021-02-02 08:47:35 -0700

    meh

--- main.go
+++ main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/turnage/graw/reddit"
+)
 
 func main() {
-	fmt.Println("hello")
+	/*model, _ := sentiment.Restore()
+
+	analysis := model.SentimentAnalysis("I like the beach", sentiment.English) // 0
+
+	fmt.Println(analysis)*/
+
+	bot, err := reddit.NewBotFromAgentFile(".secret", 0)
+
+	harvest, err := bot.Listing("/r/golang", "")
+	if err != nil {
+		fmt.Println("Failed to fetch /r/golang: ", err)
+		return
+	}
+
+	for _, post := range harvest.Posts[:5] {
+		fmt.Printf("[%s] posted [%s]\n", post.Author, post.Title)
+	}
+
 }
